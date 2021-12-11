4ea8049b3569b43ce043d3398c15fd19100af9a4
Stanislav-Kazankov <s.v.kazankov@mail.ru>
2021-12-12 00:10:24 +0900

    Корректирует исходный код подключения скрипта блока "Поделиться"

--- frontend/components/ShareBlock/ShareBlock.vue
+++ frontend/components/ShareBlock/ShareBlock.vue
@@ -24,16 +24,6 @@ export default {
             return process.env.appURL;
         },
     },
-    mounted() {
-        // скрипт подключается здесь, а не в nuxt.config.js для того,
-        // чтобы при переходе с другого лэйаута в режиме no-lazy loading
-        // блок "Поделиться" также загружался
-        const yaShareScript = document.createElement('script');
-        yaShareScript.setAttribute(
-            'src', 'https://yastatic.net/share2/share.js',
-        );
-        document.head.appendChild(yaShareScript);
-    },
 };
 </script>
 
