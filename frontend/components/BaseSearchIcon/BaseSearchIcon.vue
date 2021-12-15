65404d3c34f0fd8d999143934fd8f37601025c2e
Stanislav-Kazankov <s.v.kazankov@mail.ru>
2021-12-15 19:17:44 +0900

    Объединяет пути в иконке "Поиск"

--- frontend/components/BaseSearchIcon/BaseSearchIcon.vue
+++ frontend/components/BaseSearchIcon/BaseSearchIcon.vue
@@ -2,8 +2,7 @@
     base-svg.svg--search-icon(
         view-box="0 0 18 18"
     )
-        path(:d="pathDefinition1")
-        path(:d="pathDefinition2")
+        path(:d="pathDefinition")
 </template>
 
 <script>
@@ -12,11 +11,14 @@ import { trimEachWord } from '@/modules/stringProcessing';
 export default {
     data() {
         return {
-            pathDefinition1: trimEachWord(`
-                M7.8751 10.7021C10.6503 10.7021 12.9001 8.98415 12.9001 6.86492C12.9001 4.74569 10.6503 3.02771 7.8751 3.02771C5.09987 3.02771 2.8501 4.74569 2.8501 6.86492C2.8501 8.98415 5.09987 10.7021 7.8751 10.7021Z
-            `),
-            pathDefinition2: trimEachWord(`
+            pathDefinition: trimEachWord(`
                 M11.4751 9.6713L16.2001 13.2794
+                M7.8751 10.7021C10.6503 10.7021
+                12.9001 8.98415 12.9001 6.86492C12.9001
+                4.74569 10.6503 3.02771 7.8751
+                3.02771C5.09987 3.02771 2.8501 4.74569
+                2.8501 6.86492C2.8501 8.98415 5.09987
+                10.7021 7.8751 10.7021Z
             `),
         };
     },
